bd04aeb27343362d6241005a2e0431633a1bc457
Dwiky Fernanda <dwikyfernanda78@gmail.com>
2025-10-25 23:31:05 +0700

    Update app_streamlit.py

--- app_streamlit.py
+++ app_streamlit.py
@@ -225,9 +225,9 @@ elif selected_page == "Rekomendasi":
                                                         "Jumlah Ulasan": [row["jumlah_positif"],row["jumlah_negative"],row["jumlah_netral"]
                                                                  ]
                                                           })
-                                st.bar_chart(chart_data, x="Tipe Sentimen", y="Jumlah Ulasan", color="Tipe Sentimen")
-                                except KeyError:
-                                    st.caption("Kolom (jumlah_negatif/netral) tidak ada di CSV.")
+                                                          st.bar_chart(chart_data, x="Tipe Sentimen", y="Jumlah Ulasan", color="Tipe Sentimen")
+                            except KeyError:
+                                                          st.caption("Kolom (jumlah_negatif/netral) tidak ada di CSV.")
 
                         # ... (Kode st.link_button Anda) ...
                         if 'url_google_maps' in row and pd.notna(row['url_google_maps']) and row['url_google_maps'].startswith('http'):
@@ -376,6 +376,7 @@ elif selected_page == "About":
     ### Dataset
     * Seluruh data ulasan dan rating diambil dari **Google Maps**.
     """)
+
 
 
 
