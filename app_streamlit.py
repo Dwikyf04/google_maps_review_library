b2f7e29bd171c7665eba799897edf424c737a171
Dwiky Fernanda <dwikyfernanda78@gmail.com>
2025-10-26 03:02:48 +0700

    Update app_streamlit.py

--- app_streamlit.py
+++ app_streamlit.py
@@ -213,6 +213,20 @@ if selected_page == "Beranda":
             top_positive[['Place_name', 'city', 'rating', 'persen_positif']],
             use_container_width=True
         )
+    st.divider()
+    if not all_reviews.empty:
+        st.subheader("☁️ Word Cloud Ulasan Perpustakaan (Semua Kota)")
+        text_reviews = " ".join(all_reviews['Komentar'].astype(str))
+        if text_reviews.strip():
+            wc = WordCloud(width=800, height=500, background_color="white").generate(text_reviews)
+            
+            fig_wc, ax_wc = plt.subplots()
+            ax_wc.imshow(wc, interpolation='bilinear')
+            ax_wc.axis('off')
+            st.pyplot(fig_wc)
+        else:
+            st.caption("Tidak ada ulasan.")
+
 
 # ===============================================
 # Halaman 2: REKOMENDASI (Kode Tab 1 Lama Anda)
@@ -584,6 +598,7 @@ elif selected_page == "Feedback":
         st.balloons()
 
     
+
 
 
 
